97236132dadb0fbdc3f07d1fc102142b52954eba
jiaxv <zoujiaxv7891@gmail.com>
2025-04-01 03:43:40 +0800

    4_1_2

--- include/cipher_util.h
+++ include/cipher_util.h
@@ -7,10 +7,9 @@
 
 #include "ldacscrypto.h"
 
-void calc_hmac_uint(uint8_t *udata, size_t data_len, void *key_med, uint8_t *mac_dst, size_t mac_limit);
+typedef void (*calc_hmac_func)(uint8_t *udata, size_t data_len, void *key_med, uint8_t *mac_dst, size_t mac_limit);
 
-
-bool verify_hmac_uint(void *key_med, uint8_t *to_verify, uint8_t *udata, size_t data_len, size_t mac_limit);
+typedef bool (*verify_hmac_func)(void *key_med, uint8_t *to_verify, uint8_t *udata, size_t data_len, size_t mac_limit);
 
 
 #endif //CIPHER_UTIL_H
