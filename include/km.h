3fb963593698b46f447787e1301fd5853f0e3e40
jiaxv <zoujiaxv7891@gmail.com>
2025-03-31 19:27:52 +0800

    11.26

---
+++ include/km.h
@@ -0,0 +1,23 @@
+
+#ifndef KM_H
+#define KM_H
+
+
+/* #undef USE_SDF */
+/* #undef USE_PIICO */
+#define USE_GMSSL
+
+#ifdef USE_SDF
+
+#include <sdf/libsdf.h>
+#include <sdfkmt/sdfe-func.h>
+#include <sdfkmt/sdfe-type.h>
+#endif
+
+#ifdef USE_PIICO
+#include <piico_pc/api.h>
+#include <piico_pc/piico_define.h>
+#include <piico_pc/piico_error.h>
+#endif
+
+#endif
